fd96499b945ce9f3dc8223a9fa605c5182e87e4b
Raul Santirso Granda <rsantirso@gmail.com>
2023-01-05 17:25:54 +0100

    Fix InMemoryModel

--- Week_3/todo/src/main/scala/todo/InMemoryModel.scala
+++ Week_3/todo/src/main/scala/todo/InMemoryModel.scala
@@ -34,29 +34,34 @@ object InMemoryModel extends Model:
 
   def create(task: Task): Id =
     val id = idGenerator.nextId()
+    idStore.addOne((id,task))
     id
 
   def read(id: Id): Option[Task] =
     idStore.get(id)
 
   def complete(id: Id): Option[Task] =
-    None
+    update(id)(Task => Task.complete)
 
   def update(id: Id)(f: Task => Task): Option[Task] =
     idStore.updateWith(id)(opt => opt.map(f))
 
   def delete(id: Id): Boolean =
-    var found = false
-    found
+    idStore.remove(id).isDefined
 
   def tasks: Tasks =
     Tasks(idStore)
 
   def tags: Tags =
-    Tags(List.empty)
+    Tags(tasks
+      .toList
+      .flatMap((id, task) => task.tags)
+      .distinct)
 
   def tasks(tag: Tag): Tasks =
-    Tasks(idStore)
+    Tasks(tasks
+      .toList
+      .filter((id, task) => task.tags.contains(tag)))
 
   def clear(): Unit =
     idStore.clear()
