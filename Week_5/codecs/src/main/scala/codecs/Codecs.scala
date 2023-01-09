b121f6bab628a3d7c31ae3412e8ddebdfa2ef162
Raul Santirso Granda <rsantirso@gmail.com>
2023-01-09 13:20:51 +0100

    Define a encoder and decoder for Contacts

--- Week_5/codecs/src/main/scala/codecs/Codecs.scala
+++ Week_5/codecs/src/main/scala/codecs/Codecs.scala
@@ -291,8 +291,12 @@ trait ContactsCodecs:
   // The JSON representation of a value of type `Contacts` should be
   // a JSON object with a single field named “people” containing an
   // array of values of type `Person` (reuse the `Person` codecs)
-  given Encoder[Contacts] = ???
+  given Encoder[Contacts] =
+    ObjectEncoder.field[List[Person]]("people").transform[Contacts](_.people)
+
   // ... then implement the decoder
+  given Decoder[Contacts] =
+    Decoder.field[List[Person]]("people").transform(new Contacts(_))
 
 end ContactsCodecs
 
@@ -311,8 +315,8 @@ import scala.runtime.Nothing$
   val maybeJsonObj    = parseJson(""" { "name": "Alice", "age": 42 } """)
   val maybeJsonObj2   = parseJson(""" { "name": "Alice", "age": "42" } """)
   // Uncomment the following lines as you progress in the assignment
-   println(maybeJsonString.flatMap(_.decodeAs[Int]))
-   println(maybeJsonString.flatMap(_.decodeAs[String]))
-  // println(maybeJsonObj.flatMap(_.decodeAs[Person]))
-  // println(maybeJsonObj2.flatMap(_.decodeAs[Person]))
-  // println(renderJson(Person("Bob", 66)))
+  println(maybeJsonString.flatMap(_.decodeAs[Int]))
+  println(maybeJsonString.flatMap(_.decodeAs[String]))
+  println(maybeJsonObj.flatMap(_.decodeAs[Person]))
+  println(maybeJsonObj2.flatMap(_.decodeAs[Person]))
+  println(renderJson(Person("Bob", 66)))
